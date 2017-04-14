73fd69c3dcf3c2e87ce9bf0d98c458a82077598f
calmar2 <m_calzetta@live.com>
2017-04-14 15:30:41 +0200

    fix kafka

--- controller/kafkaCtrl.js
+++ controller/kafkaCtrl.js
@@ -6,37 +6,20 @@ exports.initKafka = initKafkaFn;
 
 
 function initKafkaFn() {
-    var topics = [ 'rank','warning_hour'
-        ,'warning_day'
-        ,'warning_hour'
-        ,'warning_week'
-        ,'hour_lamp_cons'
-        ,'day_lamp_cons'
-        ,'week_lamp_cons'
-        ,'hour_street_cons'
-        ,'day_street_cons'
-        ,'week_street_cons'
-        ,'hour_city_cons'
-        ,'day_city_cons'
-        ,'week_city_cons'];
-    var kafkaTopics = [];
-    for (var i = 0 ; i < topics.length ; i++){
-        var topic = {};
-        topic.topic = topics[i];
-        topic.partition = 0;
-        kafkaTopics.push(topic)
-    }
+
+
     var kafka = require('kafka-node'),
         Consumer = kafka.Consumer,
         client = new kafka.Client('localhost:2181'),
         consumer = new Consumer(
             client,
-            kafkaTopics
+            [{topic:'control',partition:0}]
             ,
             {
                 autoCommit: false
             }
         );
+
 
     consumer.on('message', function (message) {
         console.log(message);
