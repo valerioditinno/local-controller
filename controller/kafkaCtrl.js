3256c4b869ffefb9ec6cc29a733dfc9805c24a5c
calmar2 <m_calzetta@live.com>
2017-04-23 16:33:20 +0200

    fix kafka read

--- controller/kafkaCtrl.js
+++ controller/kafkaCtrl.js
@@ -7,28 +7,48 @@ exports.initKafka = initKafkaFn;
 
 function initKafkaFn() {
 
+    var topics = [ 'adjustment_data'];
 
+    var kafkaTopics = [];
+    for (var i = 0 ; i < topics.length ; i++){
+        var topic = {};
+        topic.topic = topics[i];
+        topic.partition = 0;
+        topic.time = -1;
+        topic.maxNum = 1;
+        kafkaTopics.push(topic)
+    }
     var kafka = require('kafka-node'),
         Consumer = kafka.Consumer,
         client = new kafka.Client('localhost:2181'),
-        consumer = new Consumer(
+        offset = new kafka.Offset(client);
+
+    offset.fetch(kafkaTopics, function (err, data) {
+        var consumerTopics = [];
+        for (var i = 0 ; i < kafkaTopics.length; i++){
+            var topic = {};
+            topic.topic = kafkaTopics[i].topic;
+            topic.partition = 0;
+            topic.offset = data[topic.topic][0][0];
+            consumerTopics.push(topic);
+        }
+        var consumer = new Consumer(
             client,
-            [{topic:'adjustment_data',partition:0}]
-            ,
+            consumerTopics,
             {
-                autoCommit: false
+                autoCommit: false,
+                fromOffset: true,
             }
         );
-
-
-    consumer.on('message', function (message) {
-        var messageObject = JSON.parse(message.value);
-        lampCtrl.updateLamp(messageObject);
+        consumer.on('message', function (message) {
+            console.log("topic : " + message.topic);
+            console.log("topic content: " + message.value);;
+        });
+        consumer.on('error',function (error) {
+            console.log(error);
+        })
     });
 
-    consumer.on('error',function (error) {
-        console.log(error);
-    });
 
 }
 
